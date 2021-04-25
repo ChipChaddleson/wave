dba4aeee33fee8b5a23aff7c11bfe61a1dd72536
ChipChaddleson <68399234+ChipChaddleson@users.noreply.github.com>
2021-04-25 14:24:10 -0400

    Add files via upload

---
+++ wave.py
@@ -0,0 +1,27 @@
+import math
+import time
+
+#discord now suports the display of files and their contenets
+#including code and its color themes
+
+
+i = 1
+j = 1
+l = 1
+leg = []
+
+#main loop
+while True:
+    sinx = round((((round(10*(math.sin(l))))+10)*3)) + 200
+    sinw = round((((round(10*(math.sin(j))))+10)*3)) + 200
+    sinn = round((((round(10*(math.sin(i))))+10)*3)) + 200
+    #this is used to generate a number (sinn) that gose up and down from 0 - 20 in the sin wave mannor
+    #it finds the sin of "i", * 10 and adds +10 (to avoid neg numbers) rounds, /2 and rounds again
+     
+    time.sleep(0.03)    #slows the program down          
+    i += 0.1
+    j += 0.2
+    l += 0.15
+    print("####".center(int(sinn)))
+    print("_-_-".center(int(sinw)))
+    print("*/*/".center(int(sinx)))
